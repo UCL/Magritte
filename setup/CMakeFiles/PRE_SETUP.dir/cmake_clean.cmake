b322a8b7d6b0832dfcb3377f0e1c5e98494b0e7e
Frederik De Ceuster <frederik.deceuster@gmail.com>
2017-09-06 15:36:25 +0100

    clean up 2

--- setup/CMakeFiles/PRE_SETUP.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PRE_SETUP"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PRE_SETUP.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
