e1bac4bf794cd205c691c545ab33dcb2dbb7ab27
Frederik De Ceuster <frederik.deceuster@gmail.com>
2018-01-30 15:00:42 +0000

    Test tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,67 +1,16 @@
 #!/bin/bash
 
 
+echo "Run the tests"
+echo "_____________"
+echo "             "
 
-echo "Log for test_ray_tracing" > test_logs/test_ray_tracing.log
-echo "++++++++++++++++++++++++" >> test_logs/test_ray_tracing.log
-echo "                        " >> test_logs/test_ray_tracing.log
+echo "Unit tests:"
 
-cd ../input
+echo "  - Test ray-tracing"
 
-python grid_1D_regular.py 250
+cd unit_tests/ray_tracing
 
-cd ../tests
+make
 
-./test_ray_tracing >> test_logs/test_ray_tracing.log
-
-
-
-echo "Log for test_level_population_solver" > test_logs/test_level_population_solver.log
-echo "++++++++++++++++++++++++++++++++++++" >> test_logs/test_level_population_solver.log
-echo "                                    " >> test_logs/test_level_population_solver.log
-
-./test_ray_tracing >> test_logs/test_level_population_solver.log
-
-
-
-echo "Log for test_feautrier" > test_logs/test_feautrier.log
-echo "++++++++++++++++++++++++++++" >> test_logs/test_feautrier.log
-echo "                            " >> test_logs/test_feautrier.log
-
-cd ../input
-
-python grid_1D_regular.py 101
-
-cd ../tests
-
-./test_feautrier >> test_logs/test_feautrier.log
-
-
-
-
-
-#   CHEMISTRY
-#--------------------------------------------------------------------------------------------------
-
-
-
-echo "Log for test_spline" > test_logs/test_spline.log
-echo "+++++++++++++++++++" >> test_logs/test_spline.log
-echo "                   " >> test_logs/test_spline.log
-
-./test_spline >> test_logs/test_spline.log
-
-
-cd test_output
-python test_spline_plot.py
-cd ..
-
-
-echo "Log for test_calc_reac_rates_rad" > test_logs/test_calc_reac_rates_rad.log
-echo "+++++++++++++++++++++++++++++++++++++++" >> test_logs/test_calc_reac_rates_rad.log
-echo "                                       " >> test_logs/test_calc_reac_rates_rad.log
-
-cd test_output
-python test_calc_reac_rates_rad_plot.py
-cd ..
-
+./test_ray_tracing.exe
