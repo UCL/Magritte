a8975acdd456f0d4a6313bae98deea25b605290c
Frederik De Ceuster <frederik.deceuster@gmail.com>
2018-07-18 14:35:16 +0100

    Added option for naming ioFolder

--- modules/RadiativeTransfer/src/folders.hpp
+++ modules/RadiativeTransfer/src/folders.hpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const string Magritte_folder = "/home/frederik/Dropbox/Astro/Magritte/modules/setup/../RadiativeTransfer/";
 
-const string  input_folder = "/home/frederik/Dropbox/Astro/MagritteProjects/Ratran/io/18-07-17_21:10:14/input/";
-const string output_folder = "/home/frederik/Dropbox/Astro/MagritteProjects/Ratran/io/18-07-17_21:10:14/output/";
+const string  input_folder = "/home/frederik/Dropbox/Astro/MagritteProjects/Spheres/nsides2/io/pit/input/";
+const string output_folder = "/home/frederik/Dropbox/Astro/MagritteProjects/Spheres/nsides2/io/pit/output/";
 
 #endif // __FOLDERS_HPP_INCLUDED__
