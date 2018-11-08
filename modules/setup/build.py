cb8dfe178ad3423fc7141dc5e3484af7362944e2
Frederik De Ceuster <frederik.deceuster@gmail.com>
2018-11-08 12:15:49 +0100

    continuous output in build module

--- modules/setup/build.py
+++ modules/setup/build.py
@@ -1,5 +1,22 @@
 #from os         import path, makedirs, getcwd
-from subprocess import check_output
+from subprocess import Popen, PIPE
+from sys        import argv
+
+
+def run(command):
+    # Run command pipe output
+    process = Popen(command, stdout=PIPE, shell=True)
+    # Continuously read output and print
+    while True:
+        # Extract output
+        line = process.stdout.readline().rstrip()
+        # Break if there is no more line
+        if not line:
+            break
+        # Convert bytelike object to string
+        line = line.decode("utf-8")
+        # Output line
+        print(line)
 
 
 #def createFolder(folder):
@@ -16,16 +33,15 @@ def compile(MagritteSetupFolder, ProjectFolder):
     command  =  'bash '
     command += f'{MagritteSetupFolder}build.sh '
     command += ProjectFolder
-    # Run command and capture output
-    output = check_output([command], shell=True)
-    # Convert bytelike object to string
-    output = output.decode("utf-8")
-    # Show output
-    print(output)
+    # Run command
+    run(command)
 
 
-#def __main__():
-#    cwd = getcwd()
-#    # Create build folder
-#    createFolder(cwd + '/build')
-#    compile()
+if __name__ == '__main__':
+    # Run compile with provided arguments  
+    if (len(argv) != 3):
+        print('ERROR: Please provide the 2 arguments:')
+        print(' 1) Full path to Magritte Setup Folder')
+        print(' 2) Full path to Project Folder')
+    else:
+        compile(argv[1],argv[2])
