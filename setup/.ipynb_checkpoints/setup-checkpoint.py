e13f5a7cdd2fc5b034b9c0e1f70f7999f43cb823
Frederik De Ceuster <frederik.deceuster@gmail.com>
2019-04-10 10:19:56 +0200

    Adding models for integration tests

--- setup/.ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,4 +0,0 @@
-# Magritte: Multidimensional Accelerated General-purpose Radiative Transfer
-#
-# Developed by: Frederik De Ceuster - University College London & KU Leuven
-# _________________________________________________________________________
