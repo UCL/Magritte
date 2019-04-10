187d2e6a7d9e797688bd4f36e1d0c8d379b34d95
Frederik De Ceuster <frederik.deceuster@gmail.com>
2019-04-10 13:55:55 +0200

    test hooks

--- scripts/convert_all_ipynb_to_py.sh
+++ scripts/convert_all_ipynb_to_py.sh
@@ -2,8 +2,12 @@
 
 # Convert all ipynb (Ipython or Jupyter notebooks) to executable python scripts.
 
+# Extract all IPython/Jupyter notebooks
 IPYNB_FILES=$(find . -type f -name "*.ipynb")
 
 for IPYNB_FILE in $IPYNB_FILES; do
-    jupyter nbconvert --to script "$IPYNB_FILE" || break
+  # If it is not a checkpoint file, convert to a python script
+  if [[ $IPYNB_FILE != *".ipynb_checkpoints"* ]]; then
+      jupyter nbconvert --to script "$IPYNB_FILE" || break
+  fi
 done
