1f5f4473caf4a9aba4a422eac388492ea1388dd3
Frederik De Ceuster <frederik.deceuster@gmail.com>
2019-04-11 16:48:53 +0200

    removed vague auto types in logger

--- configure.sh
+++ configure.sh
@@ -5,7 +5,3 @@
 # Get dependencies
 cd dependencies
 bash get_dependencies.sh
-
-# Create and activate magritte conda environment
-conda env create -f magritte_conda_environment.yml
-conda activate magritte_env
